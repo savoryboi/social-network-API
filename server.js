ab9f3711070c69990f81367017f5d0a89e965810
savoryboi <andbjerk@yahoo.com>
2022-08-16 09:39:18 -0500

    json error resolved

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const db = require('./config/connection');
 
+const { User, Thought } = require('./models')
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-const { User, Thought } = require('./models')
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.post('/users', (req, res) => {
-    res.send('yesss')
+    User.create({
+        username: req.body.username, 
+        email: req.body.email
+    }, (err, data) => {
+        if (err) throw err;
+
+        res.json(data)
+    })
 });
 
 db.once('open', () => {
